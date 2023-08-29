f2d9324ac9f857c14ba6f53da3e06f96d751916a
vhperaltaj <victor.peralta@microside.com>
2023-08-29 16:30:28 -0600

    Updated identation, added gitignore

--- Proyecto/PIC16F877A/PWM1.c
+++ Proyecto/PIC16F877A/PWM1.c
@@ -1,43 +1,37 @@
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+PWM!.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
-/*
-  AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
-  FECHA: JUNIO 2019
-*/
 /**************************************************************************
 Esta practica consiste en realizar un control PWM
 para regular la luminosidad de un led
 **************************************************************************/
 
-#include <16F877A.h>                    //Incluye el microcontrolador con el que se va a trabajar 
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F877A.h>                         //Incluye el microcontrolador con el que se va a trabajar 
+#use delay ( clock=20Mhz, crystal )          //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
 
+unsigned int16 Duty = 0;                     //Variable para guardar el valor para PWM
 
-unsigned int16 Duty=0;         //Variable para guardar el valor para PWM
-void SisInit(void) 
+void main ( void )
 {
-   setup_timer_2 (T2_DIV_BY_16, 254, 1); //Configura Timer2, periodo ajustado a 342uS
-   setup_ccp1 (CCP_PWM); //Configura el módulo CCP para uso del PWM
-}
+   setup_timer_2 ( T2_DIV_BY_16, 254, 1 );   //Configura Timer2, periodo ajustado a 342uS
+   setup_ccp1 ( CCP_PWM );                   //Configura el módulo CCP para uso del PWM
 
-void main()
-{
-   SisInit();
-
-   while (TRUE)
-   {
-      
-      for (Duty = 0; Duty < 255; Duty++)
-      {
-         set_pwm1_duty (Duty); //Asigna el valor del Duty al PWM
-         delay_ms (5); //Retardo de 5ms
+   while ( 1 ) {
+      for ( Duty = 0; Duty < 255; Duty++ ) {
+         set_pwm1_duty ( Duty );             //Asigna el valor del Duty al PWM
+         delay_ms ( 5 );                     //Retardo de 5ms
       }
-
       
-      for (Duty = 255; Duty > 0; Duty--) // Ciclo para disminuir el valor de la variable
-      {
-         set_pwm1_duty (Duty); //Asigna el valor del Duty al PWM
-         delay_ms (5); // Retardo de 5ms
+      for ( Duty = 255; Duty > 0; Duty-- ) { // Ciclo para disminuir el valor de la variable
+         set_pwm1_duty ( Duty );             //Asigna el valor del Duty al PWM
+         delay_ms ( 5 );                     // Retardo de 5ms
       }
    }
 }
-
--- Proyecto/PIC16F887/PWM1.c
+++ Proyecto/PIC16F887/PWM1.c
@@ -1,43 +1,37 @@
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+PWM1.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
-/*
-  AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
-  FECHA: JUNIO 2019
-*/
 /**************************************************************************
 Esta practica consiste en realizar un control PWM
 para regular la luminosidad de un led
 **************************************************************************/
 
-#include <16F887.h>                    //Incluye el microcontrolador con el que se va a trabajar 
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F887.h>                          //Incluye el microcontrolador con el que se va a trabajar 
+#use delay ( clock=20Mhz, crystal )          //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
 
+unsigned int16 Duty = 0;                     //Variable para guardar el valor para PWM
 
-unsigned int16 Duty=0;         //Variable para guardar el valor para PWM
-void SisInit(void) 
+void main ( void )
 {
-   setup_timer_2 (T2_DIV_BY_16, 254, 1); //Configura Timer2, periodo ajustado a 342uS
+   setup_timer_2 (T2_DIV_BY_16, 254, 1);     //Configura Timer2, periodo ajustado a 342uS
    setup_ccp1 (CCP_PWM|CCP_SHUTDOWN_AC_L|CCP_SHUTDOWN_BD_L); //Configura el módulo CCP para uso del PWM
-}
 
-void main()
-{
-   SisInit();
-
-   while (TRUE)
-   {
-      
-      for (Duty = 0; Duty < 255; Duty++)
-      {
-         set_pwm1_duty (Duty); //Asigna el valor del Duty al PWM
-         delay_ms (5); //Retardo de 5ms
+   while ( 1 ) {
+      for ( Duty = 0; Duty < 255; Duty++ ) {
+         set_pwm1_duty ( Duty );             //Asigna el valor del Duty al PWM
+         delay_ms ( 5 );                     //Retardo de 5ms
       }
-
       
-      for (Duty = 255; Duty > 0; Duty--) // Ciclo para disminuir el valor de la variable
-      {
-         set_pwm1_duty (Duty); //Asigna el valor del Duty al PWM
-         delay_ms (5); // Retardo de 5ms
+      for ( Duty = 255; Duty > 0; Duty-- ) { // Ciclo para disminuir el valor de la variable
+         set_pwm1_duty ( Duty );             //Asigna el valor del Duty al PWM
+         delay_ms ( 5 );                     // Retardo de 5ms
       }
    }
 }
-
